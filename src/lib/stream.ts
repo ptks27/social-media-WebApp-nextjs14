798e76e283b7a5fa0aee6458b8de7b9c5766da98
ptks27 <your_donotreply_email_id>
2025-05-06 12:33:19 +0700

    update bug

--- src/lib/stream.ts
+++ src/lib/stream.ts
@@ -2,7 +2,10 @@ import {StreamChat} from "stream-chat"
 
 const streamServerClient = StreamChat.getInstance(
     process.env.NEXT_PUBLIC_STREAM_KEY!,
-    process.env.STREAM_SECRET
+    process.env.STREAM_SECRET,
+    {
+    timeout: 10000, 
+  }
 )
 
 export default streamServerClient;
